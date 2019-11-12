6d4f921595d65cb26ad9f74c785cac46533d9872
rameshdeveloper62 <rameshdeveloper21@gmail.com>
2019-11-12 07:59:27 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# vuejs-crud
+First time : npm install 
+sudo npm install --no-bin-link --unsafe-perm
+sudo npm install -g node-sass@4.5.3 --unsafe-perm=true --allow-root
+sudo npm i gulp-sass -ES --unsafe-perm=true
+sudo npm install --unsafe-perm=true --allow-root
+sudo npm install node-sass@latest
+npm audit fix
+Run App : npm run watch
+Run App : npm run watch-poll
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+Route::get ( '/', function () {
+	return view ( 'welcome' );
+} );
+Route::post ( '/vueitems', 'MainController@storeItem' );
+Route::get ( '/vueitems', 'MainController@readItems' );
+Route::post ( '/vueitems/{id}', 'MainController@deleteItem' );
+Route::post ( '/edititems/{id}', 'MainController@editItem' );
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
